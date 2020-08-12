63464cc79b3b76927cd4ebca319a1e7bd0225c8c
Angelc01 <angelcevallosvillacis@gmail.com>
2020-08-12 17:57:58 -0500

    minuto 16:24 curso flutter

--- control.dart
+++ control.dart
@@ -1,7 +1,7 @@
 // eL IF es exacatament igual que en c+/Java/c#
 //h 4, m:50, s:9 ---> "04:50:09"
 
-String hhmmss(int h, int m, int s){
+String hhmmss({int h=0, int m=0, int s=0}){
 
 
   var buf = StringBuffer();
@@ -21,7 +21,50 @@ String hhmmss(int h, int m, int s){
   return buf.toString();
 }
 
+String dia2str_A(int dia){
+  String sdia;
+
+  if(dia == 1){
+    sdia='lunes';
+
+  }
+  else if(dia==2){
+    sdia='Martes';
+
+  }
+  else if(dia==3){
+    sdia='Miercoles';
+  }
+  else if(dia==4){
+    sdia="Jueves";
+  }
+  else if(dia==5){
+    sdia="Viernes";
+  }
+  else if(dia ==6){  
+    sdia="Sabado";
+  }
+  else if (dia==7){
+    sdia="Domingo";
+  }
+
+  return sdia;
+
+}
+
+const dias_semana=['domingo', 'lunes','martes', 'miercoles', 'jueves', 'viernes','sabado','domingo'];
+
+String dia2str_B(int dia){
+  if(dia>=0 && dia< dias_semana.length)
+    return dias_semana[dia];
+  else{
+      return null;
+    }
+}
+
 void main(){
 
-  print(hhmmss(4, 50, 9));
+  print(hhmmss(h: 4,m: 50));
+  print(dia2str_A(3));
+  print(dia2str_A(5));
 }
